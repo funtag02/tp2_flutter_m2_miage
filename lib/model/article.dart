a0ed7b1d620f9491455011c511c64a7ebe1d30c8
theo mac <theo.lahmar1@gmail.com>
2026-02-19 16:39:18 +0100

    add - (hard written) items list displays successfully

---
+++ lib/model/article.dart
@@ -0,0 +1,13 @@
+class Article {
+  final String title;
+  final String size;
+  final double price;
+  final String imageBase64;
+
+  Article({
+    required this.title,
+    required this.size,
+    required this.price,
+    required this.imageBase64,
+  });
+}
